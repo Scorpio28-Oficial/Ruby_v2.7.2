d825159890ad8be676d94788ef5dc34264f9cce4
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-12 17:36:50 -0600

    Update Ruby_v2.7.2.sh

--- Ruby_v2.7.2.sh
+++ Ruby_v2.7.2.sh
@@ -12,19 +12,19 @@ source ${PWD}/Colors.sh
 # FUNCIONES
 #
 function Ruby_v2.7.2 {
-sleep 0.5
+sleep 0.1
 }
 #
 # CÓDIGO
 	sleep 0.5
 	clear
 echo -e "${verde}
-██████╗ ██╗ ██╗██████╗  ██╗  ██╗
+██████╗ ██╗ ██╗██████╗  ██╗    ██╗
 ██╔══██╗██║ ██║██╔══██╗╚██╗ ██╔╝
 ██████╔╝██║ ██║██████╔╝ ╚████╔╝
 ██╔══██╗██║ ██║██╔══██╗ ╚██╔╝
-██║ ██║╚██████╔╝██████║  ██║
-╚═╝ ╚═╝ ╚═════╝ ╚═════╝  ╚═╝ v2.7.2
+██║ ██║╚██████╔╝██████╔╝ ██║
+╚═╝ ╚═╝ ╚═════╝ ╚═════╝    ╚═╝ v2.7.2
 "${blanco}
 sleep 0.5
 echo -e "${verde}
@@ -32,12 +32,13 @@ echo -e "${verde}
 █ ${blanco}INSTALANDO RUBY v2.7.2... ${verde}█
 └═══════════════════════════┘
 "${blanco}
-sleep 0.5
+sleep 0.1
 pkg unistall -y ruby &> /dev//null
-bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.22 > /dev/null 2>&1
+bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.22 &> /dev//null
 sleep 0.5
 echo -e "${verde}
 ┌═══════════════════════┐
 █ ${blanco}RUBY v2.7.2 INSTALADO ${verde}█
 └═══════════════════════┘
 "${blanco}
+cd

f020e79a75dbb05efcde87de7790b2b49a9c2271
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-12 15:54:12 -0600

    Add files via upload

---
+++ Ruby_v2.7.2.sh
@@ -0,0 +1,43 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#
+# Created by: Scorpio28
+#
+# Ruby_v2.7.2
+#
+# VARIABLES
+#
+PWD=$(pwd)
+source ${PWD}/Colors.sh
+#
+# FUNCIONES
+#
+function Ruby_v2.7.2 {
+sleep 0.5
+}
+#
+# CÓDIGO
+	sleep 0.5
+	clear
+echo -e "${verde}
+██████╗ ██╗ ██╗██████╗  ██╗  ██╗
+██╔══██╗██║ ██║██╔══██╗╚██╗ ██╔╝
+██████╔╝██║ ██║██████╔╝ ╚████╔╝
+██╔══██╗██║ ██║██╔══██╗ ╚██╔╝
+██║ ██║╚██████╔╝██████║  ██║
+╚═╝ ╚═╝ ╚═════╝ ╚═════╝  ╚═╝ v2.7.2
+"${blanco}
+sleep 0.5
+echo -e "${verde}
+┌═══════════════════════════┐
+█ ${blanco}INSTALANDO RUBY v2.7.2... ${verde}█
+└═══════════════════════════┘
+"${blanco}
+sleep 0.5
+pkg unistall -y ruby &> /dev//null
+bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.22 > /dev/null 2>&1
+sleep 0.5
+echo -e "${verde}
+┌═══════════════════════┐
+█ ${blanco}RUBY v2.7.2 INSTALADO ${verde}█
+└═══════════════════════┘
+"${blanco}

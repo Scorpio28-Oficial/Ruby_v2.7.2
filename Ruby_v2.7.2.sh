84433e6e9f0d09d58e20d25ba135f1df71b57839
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-25 21:43:32 -0600

    Update Ruby_v2.7.2.sh

--- Ruby_v2.7.2.sh
+++ Ruby_v2.7.2.sh
@@ -16,7 +16,7 @@ sleep 0.1
 }
 #
 # CÓDIGO
-	sleep 0.5
+	sleep 1
 	clear
 echo -e "${verde}
 ██████╗ ██╗   ██╗██████╗ ██╗   ██╗
@@ -27,15 +27,16 @@ echo -e "${verde}
 ╚═╝  ╚═╝ ╚═════╝ ╚═════╝    ╚═╝ v2.7.2
 "${blanco}
 sleep 0.5
+sleep 1
 echo -e "${verde}
 ┌═══════════════════════════┐
 █ ${blanco}INSTALANDO RUBY v2.7.2... ${verde}█
 └═══════════════════════════┘
 "${blanco}
-sleep 0.1
-pkg unistall -y ruby &> /dev//null
-bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.22 &> /dev//null
 sleep 0.5
+pkg unistall -y ruby &> /dev//null
+bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.2
+sleep 1
 echo -e "${verde}
 ┌═══════════════════════┐
 █ ${blanco}RUBY v2.7.2 INSTALADO ${verde}█
